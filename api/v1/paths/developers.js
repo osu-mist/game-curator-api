16d9de2ae3d228ec35607924fae6e3ad945d0be0
Alex Ruef <ruef.alex@gmail.com>
2019-07-01 16:53:36 -0700

    added getDevelopers

--- api/v1/paths/developers.js
+++ api/v1/paths/developers.js
@@ -1,3 +1,22 @@
 const appRoot = require('app-root-path');
 
 const developersDao = require('../db/json/developers-dao');
+
+const { errorHandler } = appRoot.require('errors/errors');
+const { openapi: { paths } } = appRoot.require('utils/load-openapi');
+
+/**
+ * @summary Get developers
+ */
+const get = async (req, res) => {
+    try {
+        const result = await developersDao.getDevelopers(req.query);
+        return res.send(result);
+    } catch (err) {
+        return errorHandler(res, err);
+    }
+};
+
+get.apiDoc = paths['/developers'].get;
+
+module.exports = { get };
