01dd98560a4f72a7dc4b8a9e4d0d29309ed71d81
Alex Ruef <ruef.alex@gmail.com>
2019-07-09 16:39:02 -0700

    created games serializer

---
+++ api/v1/serializers/games-serializer.js
@@ -0,0 +1,74 @@
+const appRoot = require('app-root-path');
+const JsonApiSerializer = require('jsonapi-serializer').Serializer;
+const _ = require('lodash');
+
+const { serializerOptions } = appRoot.require('utils/jsonapi');
+const { openapi } = appRoot.require('utils/load-openapi');
+const { paginate } = appRoot.require('utils/paginator');
+const { apiBaseUrl, resourcePathLink, paramsLink } = appRoot.require('utils/uri-builder');
+
+const gameResourceProp = openapi.definitions.GameResource.properties;
+const gameResourceType = gameResourceProp.type.enum[0];
+const gameResourceKeys = _.keys(gameResourceProp.attributes.properties);
+const gameResourcePath = 'games';
+const gameResourceUrl = resourcePathLink(apiBaseUrl, gameResourcePath);
+
+/**
+ * @summary Serialize gameResources to JSON API
+ * @function
+ * @param {[Object]} rawGames Raw data rows from data source
+ * @param {Object} query Query parameters
+ * @returns {Object} Serialized gameResources object
+ */
+const serializeGames = (rawGames, query) => {
+  /**
+   * Add pagination links and meta information to options if pagination is enabled
+   */
+  const pageQuery = {
+    size: query['page[size]'],
+    number: query['page[number]'],
+  };
+
+  const pagination = paginate(rawGames, pageQuery);
+  pagination.totalResults = rawGames.length;
+  rawGames = pagination.paginatedRows;
+
+  const topLevelSelfLink = paramsLink(gameResourceUrl, query);
+  const serializerArgs = {
+    identifierField: 'id',
+    resourceKeys: gameResourceKeys,
+    pagination,
+    resourcePath: gameResourcePath,
+    topLevelSelfLink,
+    query: _.omit(query, 'page[size]', 'page[number]'),
+    enableDataLinks: true,
+  };
+
+  return new JsonApiSerializer(
+    gameResourceType,
+    serializerOptions(serializerArgs),
+  ).serialize(rawGames);
+};
+
+/**
+ * @summary Serialize gameResource to JSON API
+ * @function
+ * @param {Object} rawGame Raw data row from data source
+ * @returns {Object} Serialized gameResource object
+ */
+const serializeGame = (rawGame) => {
+  const topLevelSelfLink = resourcePathLink(gameResourceUrl, rawGame.id);
+  const serializerArgs = {
+    identifierField: 'id',
+    resourceKeys: gameResourceKeys,
+    resourcePath: gameResourcePath,
+    topLevelSelfLink,
+    enableDataLinks: true,
+  };
+
+  return new JsonApiSerializer(
+    gameResourceType,
+    serializerOptions(serializerArgs),
+  ).serialize(rawGame);
+};
+module.exports = { serializeGames, serializeGame };
