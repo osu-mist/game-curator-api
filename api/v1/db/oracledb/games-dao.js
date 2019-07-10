58e35bddc4c1ba50e3a3880893ba714c4d040599
Alex Ruef <ruef.alex@gmail.com>
2019-07-10 09:57:17 -0700

    initial commit for get games by id

--- api/v1/db/oracledb/games-dao.js
+++ api/v1/db/oracledb/games-dao.js
@@ -1,6 +1,7 @@
 const appRoot = require('app-root-path');
+const _ = require('lodash');
 
-const { serializeGames } = require('../../serializers/games-serializer');
+const { serializeGame, serializeGames } = require('../../serializers/games-serializer');
 
 const conn = appRoot.require('api/v1/db/oracledb/connection');
 
@@ -18,15 +19,15 @@ const getGames = async (queries) => {
   if (queries.name) {
     sqlParams.name = queries.name;
   }
-  if (queries.developerId) {
-    sqlParams.developerId = queries.developerId;
+  if (queries.gameId) {
+    sqlParams.gameId = queries.gameId;
   }
   const sqlQuery = `
-    SELECT ID AS "id", DEVELOPER_ID AS "developerId", NAME AS "name", SCORE AS "score", RELEASE_DATE AS "releaseDate"
+    SELECT ID AS "id", DEVELOPER_ID AS "gameId", NAME AS "name", SCORE AS "score", RELEASE_DATE AS "releaseDate"
     FROM VIDEO_GAMES
     WHERE SCORE BETWEEN :scoreMin AND :scoreMax OR SCORE IS NULL
     ${sqlParams.name ? 'AND NAME = :name' : ''}
-    ${sqlParams.developerId ? 'AND DEVELOPER_ID = :developerId' : ''}
+    ${sqlParams.gameId ? 'AND DEVELOPER_ID = :gameId' : ''}
   `;
 
   const connection = await conn.getConnection();
@@ -40,4 +41,38 @@ const getGames = async (queries) => {
   }
 };
 
-module.exports = { getGames };
+/**
+ * @summary Return a specific game by unique ID
+ * @function
+ * @param {string} id Unique game ID
+ * @returns {Promise<Object>} Promise object represents a specific game or return undefined if
+ *                            term is not found
+ */
+const getGameById = async (id) => {
+  const connection = await conn.getConnection();
+  try {
+    const sqlParams = {
+      gameId: id,
+    };
+    const sqlQuery = `
+      SELECT ID AS "id", DEVELOPER_ID AS "gameId", NAME AS "name",
+      SCORE AS "score", RELEASE_DATE AS "releaseDate"
+      FROM VIDEO_GAMES
+      WHERE ID = :gameId
+    `;
+    const { rows } = await connection.execute(sqlQuery, sqlParams);
+
+    if (rows.length > 1) {
+      throw new Error('Expect a single object but got multiple results.');
+    } else if (_.isEmpty(rows)) {
+      return undefined;
+    } else {
+      const serializedGame = serializeGame(rows[0]);
+      return serializedGame;
+    }
+  } finally {
+    connection.close();
+  }
+};
+
+module.exports = { getGames, getGameById };
