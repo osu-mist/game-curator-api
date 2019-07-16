96925b67750c14aa9a787166d350490f3cb5e824
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-16 14:17:10 -0700

    initial commit for developers-dao-test.js

---
+++ tests/unit/developers-dao.test.js
@@ -0,0 +1,17 @@
+const appRoot = require('app-root-path');
+const chai = require('chai');
+// const chaiAsPromised = require('chai-as-promised');
+const config = require('config');
+const _ = require('lodash');
+// const sinon = require('sinon');
+
+// sinon.replace(config, 'get', () => ({ oracledb: {} }));
+const conn = appRoot.require('api/v1/db/oracledb/connection');
+const developersDao = appRoot.require('api/v1/db/oracledb/developers-dao');
+
+chai.should();
+// chai.use(chaiAsPromised);
+
+describe('Test the test thing', () => {
+  console.log('This test is running');
+});
