2325cfa42542cf88ecf25b4026679ace39cf6a8d
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-16 15:10:07 -0700

    added some unit test data

---
+++ tests/unit/test-data.js
@@ -0,0 +1,5 @@
+const fakeId = 'fakeId';
+const fakeBaseUrl = `/v1/developers/${fakeId}`;
+const fakeType = 'fakeType';
+const fakePath = 'fakePath';
+const fakePathUrl = `${fakeBaseUrl}/${fakePath}`;
