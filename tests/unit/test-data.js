56733b0fe2323f5197788436b707be5cf7757cfb
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-17 11:32:37 -0700

    added rawDeveloper test data

--- tests/unit/test-data.js
+++ tests/unit/test-data.js
@@ -3,3 +3,10 @@ const fakeBaseUrl = `/v1/developers/${fakeId}`;
 const fakeType = 'fakeType';
 const fakePath = 'fakePath';
 const fakePathUrl = `${fakeBaseUrl}/${fakePath}`;
+const rawDeveloper = [
+  {
+    id: 'fakeId',
+    name: 'testDeveloper',
+    website: 'http://www.example.com',
+  },
+];
