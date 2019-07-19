9116cca75d877574d99ede3b4636f73e617c1702
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-19 14:51:08 -0700

    initial commit for developers-serializer.test.js

---
+++ tests/unit/developers-serializer.test.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: 0 */
+
+const appRoot = require('app-root-path');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiExclude = require('chai-exclude');
+const config = require('config');
+const _ = require('lodash');
+const sinon = require('sinon');
+
+// sinon.replace(config, 'get', () => ({ oracledb: {} }));
+const developersSerializer = appRoot.require('api/v1/serializers/developers-serializer');
+const testData = appRoot.require('tests/unit/test-data');
+
+chai.should();
+chai.use(chaiExclude);
+chai.use(chaiAsPromised);
+
+describe('Test developers-serializer', () => {
+  const { fakeId, fakeBaseUrl } = testData;
+  const resourceSubsetSchema = (resourceType, resourceAttributes) => {
+    const schema = {
+      links: {
+        self: `${fakeBaseUrl}/${resourceType}`,
+      },
+      data: {
+        id: fakeId,
+        type: resourceType,
+        links: { self: null },
+      },
+    };
+    if (resourceAttributes) {
+      schema.data.attributes = resourceAttributes;
+    }
+    return schema;
+  };
+
+  it('test serializeDeveloper', () => {
+    const { serializeDeveloper } = developersSerializer;
+    const { rawDevelopers } = testData;
+    const resourceType = 'developer';
+
+    const serializedDeveloper = serializeDeveloper(rawDevelopers);
+    console.log(serializedDeveloper);
+  });
+});
