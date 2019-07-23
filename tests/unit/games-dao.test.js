012b6555e7ee6ba8f207dae885846c734a4e89e1
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-23 14:38:59 -0700

    initial commit for games-dao.test

---
+++ tests/unit/games-dao.test.js
@@ -0,0 +1,16 @@
+/* eslint no-unused-vars: 0 */
+
+const appRoot = require('app-root-path');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const config = require('config');
+const _ = require('lodash');
+const sinon = require('sinon');
+
+sinon.replace(config, 'get', () => ({ oracledb: {} }));
+const conn = appRoot.require('api/v1/db/oracledb/connection');
+const gamesDao = appRoot.require('api/v1/db/oracledb/games-dao');
+const developersSerializer = appRoot.require('api/v1/serializer/games-serializer');
+
+chai.should();
+chai.use(chaiAsPromised);
