c7712ea08d3b02e7247690d2a468836f782d0490
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-24 16:03:50 -0700

    initial commit for reviews-serialize,test

---
+++ tests/unit/reviews-serialize.test.js
@@ -0,0 +1,28 @@
+/* eslint no-unused-vars: 0 */
+
+const appRoot = require('app-root-path');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiSubset = require('chai-subset');
+const _ = require('lodash');
+
+const reviewsSerializer = appRoot.require('api/v1/serializers/reviews-serializer');
+const { getDefinitionProps, testSingleResource, testMultipleResources } = appRoot.require('tests/unit/serializer-test-helpers.js');
+const testData = appRoot.require('tests/unit/test-data');
+
+chai.should();
+chai.use(chaiAsPromised);
+chai.use(chaiSubset);
+const { expect } = chai;
+
+describe('Test reviews-serializer', () => {
+  const { rawReviews } = testData;
+  const resourceType = 'review';
+
+  it('test serializeGame', () => {
+    const { serializeReview } = reviewsSerializer;
+
+    const serializedReview = serializeReview(rawReviews[0]);
+    testSingleResource(serializedReview, resourceType, _.omit(rawReviews[0], ['id']));
+  });
+});
