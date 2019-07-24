76a7ebc9411d332d15547b0bc697fcd010c182a3
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-23 17:06:02 -0700

    added test for successful cases

--- tests/unit/games-dao.test.js
+++ tests/unit/games-dao.test.js
@@ -7,10 +7,41 @@ const config = require('config');
 const _ = require('lodash');
 const sinon = require('sinon');
 
-sinon.replace(config, 'get', () => ({ oracledb: {} }));
+// sinon.replace(config, 'get', () => ({ oracledb: {} }));
 const conn = appRoot.require('api/v1/db/oracledb/connection');
 const gamesDao = appRoot.require('api/v1/db/oracledb/games-dao');
 const gamesSerializer = appRoot.require('api/v1/serializers/games-serializer');
 
 chai.should();
 chai.use(chaiAsPromised);
+
+describe('Test games-dao', () => {
+  afterEach(() => sinon.restore());
+
+  const createConnStub = executeReturn => sinon.stub(conn, 'getConnection').resolves({
+    execute: () => executeReturn,
+    close: () => null,
+  });
+
+
+  it('getDevelopers should be fulfilled', () => {
+    const testCases = [
+      { testCase: [{}, {}] },
+      { testCase: [{}] },
+    ];
+    sinon.stub(gamesSerializer, 'serializeGames').returnsArg(0);
+
+    const fulfilledPromises = [];
+    _.forEach(testCases, ({ testCase }) => {
+      const connStub = createConnStub({ rows: testCase });
+
+      const result = gamesDao.getGames('fakeId');
+      fulfilledPromises.push(result.should
+        .eventually.be.fulfilled
+        .and.deep.equals(testCase));
+
+      connStub.restore();
+    });
+    return Promise.all(fulfilledPromises);
+  });
+
