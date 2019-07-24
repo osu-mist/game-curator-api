8a3d00713555ce5e73db03edda6b7fb762782b1d
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-24 11:29:07 -0700

    removed unused vars

--- tests/unit/games-dao.test.js
+++ tests/unit/games-dao.test.js
@@ -1,13 +1,9 @@
-/* eslint no-unused-vars: 0 */
-
 const appRoot = require('app-root-path');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-const config = require('config');
 const _ = require('lodash');
 const sinon = require('sinon');
 
-// sinon.replace(config, 'get', () => ({ oracledb: {} }));
 const conn = appRoot.require('api/v1/db/oracledb/connection');
 const gamesDao = appRoot.require('api/v1/db/oracledb/games-dao');
 const gamesSerializer = appRoot.require('api/v1/serializers/games-serializer');
