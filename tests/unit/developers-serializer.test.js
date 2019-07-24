36132b30616fccdfd7d11e55be96132f3f040b52
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-24 11:43:38 -0700

    created separate class to help test serializers. Moved functions from developers-serializer.test into the new class.

--- tests/unit/developers-serializer.test.js
+++ tests/unit/developers-serializer.test.js
@@ -6,8 +6,8 @@ const _ = require('lodash');
 
 // sinon.replace(config, 'get', () => ({ oracledb: {} }));
 const developersSerializer = appRoot.require('api/v1/serializers/developers-serializer');
+const { getDefinitionProps, testSingleResource, testMultipleResources } = appRoot.require('tests/unit/serializer-test-helpers.js');
 const testData = appRoot.require('tests/unit/test-data');
-const { openapi } = appRoot.require('utils/load-openapi');
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -15,78 +15,6 @@ chai.use(chaiSubset);
 const { expect } = chai;
 
 describe('Test developers-serializer', () => {
-  const { fakeId, fakeBaseUrl } = testData;
-
-  const resourceSubsetSchema = (resourceType, resourceAttributes) => {
-    const schema = {
-      links: {
-        self: `${fakeBaseUrl}`,
-      },
-      data: {
-        id: fakeId,
-        type: resourceType,
-        links: { self: `${fakeBaseUrl}` },
-      },
-    };
-    if (resourceAttributes) {
-      schema.data.attributes = resourceAttributes;
-    }
-    return schema;
-  };
-
-  /**
-   * @summary Helper function for lite-testing single resource
-   * @function
-   * @param {object} serializedResource serialized resource
-   * @param {string} resourceType resource type
-   * @param {object} nestedProps object containing properties nested under data.attributes
-   */
-  const testSingleResource = (serializedResource, resourceType, nestedProps) => {
-    expect(serializedResource).to.containSubset(resourceSubsetSchema(resourceType, nestedProps));
-
-    if (nestedProps) {
-      _.forEach(Object.keys(nestedProps), (prop) => {
-        expect(serializedResource).to.have.nested.property(`data.attributes.${prop}`);
-      });
-    }
-  };
-
-  /**
-   * @summary Helper function for lite-testing multiple resources
-   * @function
-   * @param {Object} serializedResources serialized resources
-   * @returns {Object} data object from serialized resources for further use
-   */
-  const testMultipleResources = (serializedResources) => {
-    const serializedResourcesData = serializedResources.data;
-    expect(_.omit(serializedResources, 'meta')).to.have.keys('data', 'links');
-    expect(serializedResourcesData).to.be.an('array');
-
-    return serializedResourcesData;
-  };
-
-  /**
-   * @summary Helper function to get definition from openapi specification
-   * @function
-   * @param {string} definition the name of definition
-   * @param {Object} nestedOption nested option
-   * @param {boolean} nestedOption.dataItem a boolean which represents whether it's a data item
-   * @param {string} nestedOption.dataField data field name
-   * @returns {Object}
-   */
-  const getDefinitionProps = (definition, nestedOption) => {
-    let result = openapi.definitions[definition].properties;
-    if (nestedOption) {
-      const { dataItem, dataField } = nestedOption;
-      if (dataItem) {
-        result = result.data.items.properties.attributes.properties;
-      } else if (dataField) {
-        result = result.data.properties.attributes.properties[dataField].items.properties;
-      }
-    }
-    return result;
-  };
-
   it('test serializeDeveloper', () => {
     const { serializeDeveloper } = developersSerializer;
     const { rawDevelopers } = testData;
---
+++ tests/unit/serializer-test-helpers.js
@@ -0,0 +1,83 @@
+
+const appRoot = require('app-root-path');
+const { expect } = require('chai');
+const _ = require('lodash');
+
+const { openapi } = appRoot.require('utils/load-openapi');
+const { fakeId, fakeBaseUrl } = appRoot.require('tests/unit/test-data');
+
+const resourceSubsetSchema = (resourceType, resourceAttributes) => {
+  const schema = {
+    links: {
+      self: `${fakeBaseUrl}`,
+    },
+    data: {
+      id: fakeId,
+      type: resourceType,
+      links: { self: `${fakeBaseUrl}` },
+    },
+  };
+  if (resourceAttributes) {
+    schema.data.attributes = resourceAttributes;
+  }
+  return schema;
+};
+
+/**
+ * @summary Helper function for lite-testing single resource
+ * @function
+ * @param {object} serializedResource serialized resource
+ * @param {string} resourceType resource type
+ * @param {object} nestedProps object containing properties nested under data.attributes
+ */
+const testSingleResource = (serializedResource, resourceType, nestedProps) => {
+  expect(serializedResource).to.containSubset(resourceSubsetSchema(resourceType, nestedProps));
+
+  if (nestedProps) {
+    _.forEach(Object.keys(nestedProps), (prop) => {
+      expect(serializedResource).to.have.nested.property(`data.attributes.${prop}`);
+    });
+  }
+};
+
+/**
+ * @summary Helper function for lite-testing multiple resources
+ * @function
+ * @param {Object} serializedResources serialized resources
+ * @returns {Object} data object from serialized resources for further use
+ */
+const testMultipleResources = (serializedResources) => {
+  const serializedResourcesData = serializedResources.data;
+  expect(_.omit(serializedResources, 'meta')).to.have.keys('data', 'links');
+  expect(serializedResourcesData).to.be.an('array');
+
+  return serializedResourcesData;
+};
+
+/**
+ * @summary Helper function to get definition from openapi specification
+ * @function
+ * @param {string} definition the name of definition
+ * @param {Object} nestedOption nested option
+ * @param {boolean} nestedOption.dataItem a boolean which represents whether it's a data item
+ * @param {string} nestedOption.dataField data field name
+ * @returns {Object}
+ */
+const getDefinitionProps = (definition, nestedOption) => {
+  let result = openapi.definitions[definition].properties;
+  if (nestedOption) {
+    const { dataItem, dataField } = nestedOption;
+    if (dataItem) {
+      result = result.data.items.properties.attributes.properties;
+    } else if (dataField) {
+      result = result.data.properties.attributes.properties[dataField].items.properties;
+    }
+  }
+  return result;
+};
+
+module.exports = {
+  testSingleResource,
+  testMultipleResources,
+  getDefinitionProps,
+};
