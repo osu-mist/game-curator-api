3af35390dd2d8610f1d0af774bcfcc6ab8276f78
Alex Ruef <alex.ruef@oregonstate.edu>
2019-07-30 11:59:56 -0700

    added calls to assert_data_returned wherever necessary

--- tests/integration/integration-test.py
+++ tests/integration/integration-test.py
@@ -38,13 +38,14 @@ class integration_tests(unittest.TestCase):
     def assert_data_returned(self, test_case, response_data):
         error_message = (f'No data returned from server.'
                          f' Check that \'{test_case}\' in configuration.json'
-                         f' contains valid data')
+                         f' contains valid data.')
         self.assertGreater(len(response_data), 0, error_message)
 
     # Test case: GET /developers/{developerId}
     def test_get_developer_by_id(self):
         resource = 'DeveloperResource'
-        for developer_id in self.test_cases['valid_developer_ids']:
+        current_test_case = 'valid_developer_ids'
+        for developer_id in self.test_cases[current_test_case]:
             with self.subTest('Test valid developer Ids',
                               developer_id=developer_id):
                 response = utils.test_endpoint(self,
@@ -55,7 +56,8 @@ class integration_tests(unittest.TestCase):
                 actual_developer_id = response_data['id']
                 self.assertEqual(actual_developer_id, developer_id)
 
-        for developer_id in self.test_cases['non_existant_developer_ids']:
+        current_test_case = 'non_existant_developer_ids'
+        for developer_id in self.test_cases[current_test_case]:
             with self.subTest('Test invalid developer Ids',
                               developer_id=developer_id):
                 utils.test_endpoint(self,
@@ -63,7 +65,8 @@ class integration_tests(unittest.TestCase):
                                     'Error',
                                     404)
 
-        for developer_id in self.test_cases['invalid_developer_ids']:
+        current_test_case = 'invalid_developer_ids'
+        for developer_id in self.test_cases[current_test_case]:
             with self.subTest('Test invalid developer Ids',
                               developer_id=developer_id):
                 utils.test_endpoint(self,
@@ -74,7 +77,8 @@ class integration_tests(unittest.TestCase):
     # Test case: GET /developers
     def test_get_developers(self):
         resource = 'DeveloperResource'
-        for developer_name in self.test_cases['developer_names']:
+        current_test_case = 'developer_names'
+        for developer_name in self.test_cases[current_test_case]:
             with self.subTest('Test name query parameter',
                               developer_name=developer_name):
                 params = {'name': developer_name}
@@ -84,6 +88,7 @@ class integration_tests(unittest.TestCase):
                                                200,
                                                query_params=params)
                 response_data = response.json()['data']
+                self.assert_data_returned(current_test_case, response_data)
                 for row in response_data:
                     returned_name = row['attributes']['name']
                     self.assertEqual(developer_name, returned_name)
@@ -92,7 +97,8 @@ class integration_tests(unittest.TestCase):
     def test_get_games_by_id(self):
         resource = 'GameResource'
         nullable_fields = ['score']
-        for game_id in self.test_cases['valid_game_ids']:
+        current_test_case = 'valid_game_ids'
+        for game_id in self.test_cases[current_test_case]:
             with self.subTest('Test valid game ids', game_id=game_id):
                 response = utils.test_endpoint(self,
                                                f'/games/{game_id}',
@@ -103,7 +109,8 @@ class integration_tests(unittest.TestCase):
                 returned_game_id = response_data['id']
                 self.assertEqual(returned_game_id, game_id)
 
-        for game_id in self.test_cases['non_existant_game_ids']:
+        current_test_case = 'non_existant_game_ids'
+        for game_id in self.test_cases[current_test_case]:
             with self.subTest('Test non existant game ids', game_id=game_id):
                 response = utils.test_endpoint(self,
                                                f'/games/{game_id}',
@@ -111,7 +118,8 @@ class integration_tests(unittest.TestCase):
                                                404,
                                                nullable_fields=nullable_fields)
 
-        for game_id in self.test_cases['invalid_game_ids']:
+        current_test_case = 'invalid_game_ids'
+        for game_id in self.test_cases[current_test_case]:
             with self.subTest('Test invalid game ids', game_id=game_id):
                 response = utils.test_endpoint(self,
                                                f'/games/{game_id}',
@@ -124,7 +132,8 @@ class integration_tests(unittest.TestCase):
         path = '/games'
         resource = 'GameResource'
         nullable_fields = ['score']
-        for game_name in self.test_cases['game_names']:
+        current_test_case = 'game_names'
+        for game_name in self.test_cases[current_test_case]:
             with self.subTest('Test name query parameter',
                               game_name=game_name):
                 params = {'name': game_name}
@@ -135,11 +144,13 @@ class integration_tests(unittest.TestCase):
                                                query_params=params,
                                                nullable_fields=nullable_fields)
                 response_data = response.json()['data']
+                self.assert_data_returned(current_test_case, response_data)
                 for row in response_data:
                     returned_name = row['attributes']['name']
                     self.assertEqual(game_name, returned_name)
 
-        for developer_id in self.test_cases['valid_developer_ids']:
+        current_test_case = 'game_developer_ids'
+        for developer_id in self.test_cases[current_test_case]:
             with self.subTest('Test developerId query parameter',
                               developer_id=developer_id):
                 params = {'developerId': developer_id}
@@ -150,11 +161,13 @@ class integration_tests(unittest.TestCase):
                                                query_params=params,
                                                nullable_fields=nullable_fields)
                 response_data = response.json()['data']
+                self.assert_data_returned(current_test_case, response_data)
                 for row in response_data:
                     returned_id = row['attributes']['developerId']
                     self.assertEqual(developer_id, returned_id)
 
-        for score_min in self.test_cases['scores']:
+        current_test_case = 'scores'
+        for score_min in self.test_cases[current_test_case]:
             with self.subTest('Test scoreMin query parameter',
                               score_min=score_min):
                 params = {'scoreMin': score_min}
@@ -165,11 +178,12 @@ class integration_tests(unittest.TestCase):
                                                query_params=params,
                                                nullable_fields=nullable_fields)
                 response_data = response.json()['data']
+                self.assert_data_returned(current_test_case, response_data)
                 for row in response_data:
                     returned_score = row['attributes']['score']
                     self.assertLessEqual(int(score_min), returned_score)
 
-        for score_max in self.test_cases['scores']:
+        for score_max in self.test_cases[current_test_case]:
             with self.subTest('Test scoreMax query parameter',
                               score_max=score_max):
                 params = {'scoreMax': score_max}
@@ -180,6 +194,7 @@ class integration_tests(unittest.TestCase):
                                                query_params=params,
                                                nullable_fields=nullable_fields)
                 response_data = response.json()['data']
+                self.assert_data_returned(current_test_case, response_data)
                 for row in response_data:
                     returned_score = row['attributes']['score']
                     self.assertGreaterEqual(int(score_max), returned_score)
@@ -187,7 +202,8 @@ class integration_tests(unittest.TestCase):
     # Test case: GET /reviews/{reviewId}
     def test_get_reviews_by_id(self):
         resource = 'ReviewResource'
-        for review_id in self.test_cases['valid_review_ids']:
+        current_test_case = 'valid_review_ids'
+        for review_id in self.test_cases[current_test_case]:
             with self.subTest('Test valid review ids', review_id=review_id):
                 response = utils.test_endpoint(self,
                                                f'/reviews/{review_id}',
@@ -197,7 +213,8 @@ class integration_tests(unittest.TestCase):
                 returned_id = response_data['id']
                 self.assertEqual(returned_id, review_id)
 
-        for review_id in self.test_cases['non_existant_review_ids']:
+        current_test_case = 'non_existant_review_ids'
+        for review_id in self.test_cases[current_test_case]:
             with self.subTest('Test non existant review ids',
                               review_id=review_id):
                 response = utils.test_endpoint(self,
@@ -205,7 +222,8 @@ class integration_tests(unittest.TestCase):
                                                'Error',
                                                404)
 
-        for review_id in self.test_cases['invalid_review_ids']:
+        current_test_case = 'invalid_review_ids'
+        for review_id in self.test_cases[current_test_case]:
             with self.subTest('Test invalid review ids',
                               review_id=review_id):
                 response = utils.test_endpoint(self,
